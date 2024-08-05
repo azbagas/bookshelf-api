be358bb5d8382e9e6077895334eb3d1f432dfc03
azbagas <azharbagaskara1@gmail.com>
2024-08-05 12:11:38 +0700

    completed

--- src/server.js
+++ src/server.js
@@ -1,1 +1,21 @@
-console.log('Hello World');
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 9000,
+    host: process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
